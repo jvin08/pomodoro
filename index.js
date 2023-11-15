20579ae944c90ffb2c54d1b1cbbd0b3b8f2ac1fc
Ivan Vasylyniuk <jvin08@gmail.com>
2023-11-15 16:01:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ const localStorageData = JSON.parse(localStorage.getItem('myPomidoro'));
 if(!localStorageData){
     localStorage.setItem('myPomidoro', JSON.stringify([[],[]]))
 } else {
-  console.log('storage myPomidoro exists!');
+  console.log('storage myPomidoro exists!')
 }
-const pendingTasks = localStorageData[0];
-const finishedTasks = localStorageData[1];
+const pendingTasks = localStorageData[0]
+let currentTask
+let taskSetUp = false
+if(pendingTasks.length){
+  currentTask = pendingTasks[0]
+}
+const finishedTasks = localStorageData[1]
 
 // const  = document.querySelector('.')
 const themeColors = ["RoyalBlue","Gold","DarkGreen", "#222831", "DarkOrange"]
@@ -29,6 +34,7 @@ const finishedEl = document.querySelector('.finished-list')
 const secondsEl = document.querySelector('.seconds-arrow')
 const minutesEl = document.querySelector('.minutes-arrow')
 const timerEl = document.querySelector('.timer')
+const breakEl = document.querySelector('.break')
 const playEl = document.querySelector('.play')
 const pauseEl = document.querySelector('.pause')
 const stopEl = document.querySelector('.stop')
@@ -40,9 +46,16 @@ const tinePluckEl = document.querySelector('.tine-pluck')
 const pipeBlowEl = document.querySelector('.pipe-blow')
 const hitHighEl = document.querySelector('.hit-high')
 const pianoSoundEl = document.querySelector('.piano-sound')
-
+const startHereArrowEl = document.querySelector('.start-here')
+const fakerColorBtnEl = document.querySelector('.faker-color')
+//time settings elements
+let pomodoroEl = document.querySelector('.pomodoro')
+let shortEl = document.querySelector('.short')
+let longEl = document.querySelector('.long')
+let cyclesEl = document.querySelector('.cycles')
 const testConfettiEl = document.querySelector('.one')
 const clockfaceEl = document.querySelector('.timer-bg')
+const cycleTrackerEl = document.querySelector('.cycle-tracker')
 
 const tinePluckSound = new Audio('https://raw.githubusercontent.com/jvin08/sounds/master/_tine_pluck.mp3');
 const hitHighSound = new Audio('https://raw.githubusercontent.com/jvin08/sounds/master/hit_high.mp3');
@@ -50,7 +63,7 @@ const PianoSound = new Audio('https://raw.githubusercontent.com/jvin08/sounds/ma
 const PipeBlowSound = new Audio('https://raw.githubusercontent.com/jvin08/sounds/master/pipe_blow.mp3');
 const CheeringSound = new Audio('https://raw.githubusercontent.com/jvin08/sounds/master/cheering.mp3')
 
-
+//colorfull themes creating
 for (let i = 0; i < themeEl.children.length; i++) {
 themeEl.children[i].addEventListener('click', (event) => {
     document.body.style.backgroundColor = themeColors[i];
@@ -69,16 +82,24 @@ tinePluckEl.addEventListener('click', ()=>{tinePluckSound.play()})
 hitHighEl.addEventListener('click', ()=>{hitHighSound.play()})
 pianoSoundEl.addEventListener('click', ()=>{PianoSound.play()})
 pipeBlowEl.addEventListener('click', ()=>{PipeBlowSound.play()})
-testConfettiEl.addEventListener('click', lunchSession)
+testConfettiEl.addEventListener('click', lunchConfettiSession)
+fakerColorBtnEl.addEventListener('click', changeBodyColorWithFaker)
 
 renderPending()
 renderFinished()
 
 function toggleModal(){
-  modalEl.classList.toggle('hidden')
+   modalEl.classList.toggle('hidden')
 }
 function toggleTimeSetUpModal(){
-  timeSetupModalEl.classList.toggle('hidden')
+  if(taskSetUp && !timerIsActive){
+    timeSetupModalEl.classList.toggle('hidden')
+  } else {
+    toggleTasksArrowBlinking()
+    setTimeout(()=>{
+      toggleTasksArrowBlinking()
+    }, 1000)
+  }
 }
 
 function createTaskElement(task, eventHandler, parent){
@@ -90,7 +111,9 @@ function createTaskElement(task, eventHandler, parent){
   trashEl.className = "trash"
   taskEl.id = task.uuid
   if(parent.className==='started-list'){
+    taskEl.draggable = 'true'
     taskEl.addEventListener('click', addTaskToScreen)
+    
     trashEl.style.display = "none"
   }
   if(parent.className==='finished-list'){
@@ -99,7 +122,6 @@ function createTaskElement(task, eventHandler, parent){
   }
   taskEl.textContent = task.taskText
   taskEl.append(trashEl)
-  taskEl.draggable = 'true'
   parent.append(taskEl)
   return taskEl
 }
@@ -107,9 +129,13 @@ function createTaskElement(task, eventHandler, parent){
 function changeBodyColor(color){
   document.querySelector('body').style.backgroundColor = color
 }
+function changeBodyColorWithFaker(){
+  changeBodyColor(faker.color.rgb())
+}
 
 function addTaskToScreen(e){
   currentTaskEl.innerText = e.target.innerText
+  placeTaskInProgress(e.target.id)
   const messageEl = document.querySelector('.message-pending')
   messageEl.innerText = 'You can see it now on dashboard'
   messageEl.style.color = 'red'
@@ -119,6 +145,21 @@ function addTaskToScreen(e){
   }, 2000)
   taskSetUp = true
 }
+//---------PUT-----CURRENT--------TASK------IN----PROGRESS------
+function placeTaskInProgress(id){
+  let taskIndex = pendingTasks.findIndex(task => id === task.uuid )
+  let taskGoingToProgress = pendingTasks.splice(taskIndex, 1)
+  pendingTasks.unshift(taskGoingToProgress[0])
+  localStorage.setItem('myPomidoro', JSON.stringify(localStorageData))
+  renderPending()
+  currentTask = pendingTasks[0]
+  // updateTimeSettings(currentTask)
+  minutes = currentTask.cyclesArray[0]
+  let cycle = 1
+  let cycles = Math.ceil(currentTask.cyclesArray.length/2)
+  update(0, minutes, cycle, cycles)
+}
+
 
 //creating new task
 function handleEnterBtn(){
@@ -126,6 +167,8 @@ function handleEnterBtn(){
     const newTask =  {
       taskText: inputFieldEl.value,
       uuid: uuidv4(),
+      currentCycle: 0,
+      cyclesArray: [2,1,2]
     }
     addToLocalStorage(newTask)
     }  
@@ -148,16 +191,21 @@ document.ondrop = function(event) {
 };
 
 function pendingTaskHandler(e){
-  // let id = e.target.id || e.target.parentElement.id
   const id = event.dataTransfer.getData("html");
-  console.log(id)
-  //add task to finished Tasks list
   let removedTaskIndex = pendingTasks.findIndex(task => id === task.uuid )
-  let taskMovingToFinished = pendingTasks[removedTaskIndex]
+  removeTaskToTrash(removedTaskIndex)
+}
+  function removeTaskToTrash(index){
+  //add task to finished Tasks list
+  let taskMovingToFinished = pendingTasks[index]
   finishedTasks.unshift(taskMovingToFinished)
   //remove task from pending Tasks list
-  pendingTasks.splice(removedTaskIndex, 1)
+  pendingTasks.splice(index, 1)
   localStorage.setItem('myPomidoro', JSON.stringify(localStorageData))
+  if(!pendingTasks.length){
+    currentTaskEl.innerText = '...No...current...tasks...'
+    taskSetUp = false
+  }
   renderPending()
   renderFinished()
 }
@@ -165,7 +213,6 @@ function pendingTaskHandler(e){
 function deleteTaskCompletely(e){
   let id = e.target.id || e.target.parentElement.id
   let taskToBeDeletedIndex = finishedTasks.findIndex(task => task.uuid === e.target.id)
-  console.log('taskToBeDeletedIndex: ' + taskToBeDeletedIndex)
   finishedTasks.splice(taskToBeDeletedIndex, 1)
   localStorage.setItem('myPomidoro', JSON.stringify(localStorageData))
   renderFinished()
@@ -173,7 +220,7 @@ function deleteTaskCompletely(e){
 
 function addToLocalStorage(task){
   //adding new task to pending storage
-  pendingTasks.unshift(task)
+  pendingTasks.push(task)
   localStorage.setItem('myPomidoro', JSON.stringify(localStorageData))
 }
 
@@ -186,7 +233,7 @@ function renderPending(){
 }
 
 function renderFinished(){
-  finishedEl.innerHTML = '<p class="trash-table">Place trash here</p>'
+  finishedEl.innerHTML = '<p class="trash-table">Place pending here</p>'
   //render if tasks exist
   finishedTasks.length && finishedTasks.forEach((task)=>{
     // task.color = 'gray'
@@ -213,7 +260,7 @@ function handleKeyPress(e) {
     renderPending()
   }
 }
-
+//flipping buttons for fun
 pipeBlowEl.addEventListener("click", function(){
     pipeBlowEl.style.top = "410px"
     tinePluckEl.style.top = "45px"
@@ -227,14 +274,10 @@ let stopWatch
 let arrowStopWatch
 let taskBlinking
 let seconds = 60
-let pomodoro
-let minutes = pomodoro || 20
-let taskSetUp = false
-let pomoCount = 4 //updates every 4 Pomodoro
-let shortBreak
-let longBreak
-let cycles
+let minutes = 2
+let currentCycle = 0
 
+taskBlinking = setInterval(toggleTaskBlinking, 1000)
 // timeIntervalEvents
 function startArrowWatch(){
   arrowStopWatch = setInterval(spinArrows,1000)
@@ -242,42 +285,65 @@ function startArrowWatch(){
 //array with elements to shake
 const timerElementsEl = [clockfaceEl, playEl, pauseEl, stopEl, timerEl]
 let timerIsActive = false
+let timerIsOnPause = true
 // --------------------------------START TIMER----------------
-
 function startTimer() {
-  if(taskSetUp && !timerIsActive){
+  if(taskSetUp && !timerIsActive && timerIsOnPause){
     timerIsActive = true
-    stopWatch = setInterval(myTimer,1000)
-    arrowStopWatch = setInterval(spinArrows,1000)
-    clearInterval(taskBlinking)//for pauseEvent purpose
-    taskBlinking = setInterval(toggleTaskBlinking, 500)
+    timerIsOnPause = false
+    stopWatch = setInterval(myTimer,50)
+    arrowStopWatch = setInterval(spinArrows,50)
     for(const element of timerElementsEl){
       element.classList.add('shake')
-      element.style.animationDuration = '5s';
+      element.style.animationDuration = '5s'
     }
   } 
   if(!taskSetUp){
-    currentTaskEl.innerText = '...Set up your task first...'
+    toggleTasksArrowBlinking()
+    setTimeout(()=>{
+      toggleTasksArrowBlinking()
+    }, 1000)
+    currentTaskEl.innerText = '...No...current...tasks...'
   }
 }
 
 function toggleTaskBlinking(){
   currentTaskEl.classList.toggle('transparent')
 }
+function toggleTasksArrowBlinking(){
+  startHereArrowEl.classList.toggle('visible')
+}
+function toggleBreak(){
+  breakEl.classList.toggle('visible')
+}
+
 
 function myTimer() {
-  seconds--
-  if(seconds < 0){
-  	seconds = 59
+    seconds--
+    if(seconds < 0){
+      seconds = 59
+    }
+    seconds === 59 && minutes--
+  if(minutes === 0 && seconds === 0){
+    toggleBreak()
+    currentCycle++
+    if(currentTask.cyclesArray.length > currentCycle){
+       minutes = currentTask.cyclesArray[currentCycle]
+       startTimer()
+    } 
+    if(currentTask.cyclesArray.length === currentCycle){
+      stopTimer()
+      lunchConfettiSession()
+    }
   }
-  seconds === 59 && minutes--
-  let showSeconds = seconds<10 ? `0${seconds}` : `${seconds}`
-  let showMinutes = minutes<10 ? `0${minutes}` : `${minutes}`
-  timerEl.innerHTML = showMinutes + ':' + showSeconds
+  let cycles = Math.ceil(currentTask.cyclesArray.length/2)
+  let cycle = Math.ceil((currentCycle + 1)/2)
+  update(seconds, minutes, cycle, cycles)
 }
 
 function pauseTimer(){
   timerIsActive=false
+  timerIsOnPause=true
   clearInterval(stopWatch)
   clearInterval(arrowStopWatch)
   for(const element of timerElementsEl){//array with elements to shake
@@ -286,60 +352,97 @@ function pauseTimer(){
 }
 
 function stopTimer(){
-  taskSetUp = false
-  timerIsActive=false
-  clearInterval(stopWatch)
-  clearInterval(arrowStopWatch)
-  clearInterval(taskBlinking)
-  seconds = 60
-  minutes = 25
-  degrees = 0
-  resetArrows(degrees)
-  secondsEl.style.transform = `rotate(${degrees}deg)`;
-  
-  timerEl.innerText = '25:00'
-  currentTaskEl.innerText = '...task in progress will see here...'
-  if(currentTaskEl.style.opacity === '0'){
-    toggleTaskBlinking()
-  }
-  for(const element of timerElementsEl){
-      element.classList.remove('shake')
+  if(timerIsActive || timerIsOnPause){
+    timerIsActive=false
+    timerIsOnPause=true
+    clearInterval(stopWatch)
+    clearInterval(arrowStopWatch)
+    seconds = 0
+    minutes = 0
+    degrees = 0
+    resetArrows(degrees)
+    taskSetUp = false
+    secondsEl.style.transform = `rotate(${degrees}deg)`
+    update(seconds, minutes, 0, 0)//zeros for current cycle and total cycles 
+    for(const element of timerElementsEl){
+        element.classList.remove('shake')
+    }
+    currentTaskEl.textContent = '...No...current...tasks...'
+    currentCycle=0
+  } else {
+    toggleTasksArrowBlinking()
+    setTimeout(()=>{
+      toggleTasksArrowBlinking()
+    }, 1000)
   }
 }
 
+function update(seconds, minutes, cycle, cycles){
+  cycleTrackerEl.innerText = `${cycle} of ${cycles}`
+  let showSeconds = seconds<10 ? `0${seconds}` : `${seconds}`
+  let showMinutes = minutes<10 ? `0${minutes}` : `${minutes}`
+  timerEl.innerHTML = showMinutes + ':' + showSeconds
+}
+
+
 function collectTimeSettings() {
-  let pomodoroEl = document.querySelector('.pomodoro')
-  let shortEl = document.querySelector('.short')
-  let longEl = document.querySelector('.long')
-  let cyclesEl = document.querySelector('.cycles')
- minutes = parseInt(pomodoroEl.value)
-  timerEl.textContent = minutes < 10 ? `0${minutes}:00` : `${minutes}:00`
-  shortBreak = shortEl.value
-  longBreak = longEl.value
-  cycles = cyclesEl.value
+  minutes = parseInt(pomodoroEl.value)
+  let shortBreak = parseInt(shortEl.value)
+  let longBreak = parseInt(longEl.value)
+  let cycles = parseInt(cyclesEl.value)
+  let currentCycle = 1
+  //form cycles array
+  currentTask.cyclesArray = cyclesArray(minutes, shortBreak, longBreak, cycles)
+  update(0, minutes, currentCycle, cycles)
+  localStorage.setItem('myPomidoro', JSON.stringify(localStorageData))
+}
+
+function cyclesArray(pomodoro, shortBreak, longBreak, cycles){
+  let cyclesArray = []
+  // cycles = cycles - cycles % 2
+  for(let i=0;i<cycles;i++){
+    cyclesArray.push(pomodoro)
+    if(i !== cycles - 1){
+      if((i+1) % 4 === 0){
+        cyclesArray.push(longBreak)
+      } else {
+        cyclesArray.push(shortBreak)
+      }
+    }
+  }
+  return cyclesArray
+}
+
+
+function updateTimeSettings(currentTask){
+  pomodoroEl.value = currentTask.cyclesArray[0]
+  shortEl.value = currentTask.cyclesArray[1]
+  // longEl.value = longBreak
+  // cyclesEl.value = cycles
 }
 
 
 // Generate random task
-const taskGeneratorBtnEl = document.querySelector('.task-generator');
+const taskGeneratorBtnEl = document.querySelector('.task-generator')
 const fetchBoredActivity = async () => {
   try {
-    const response = await fetch('https://www.boredapi.com/api/activity');
-    const data = await response.json();
+    const response = await fetch('https://www.boredapi.com/api/activity')
+    const data = await response.json()
     console.log('fetching...')
     if (response.status !== 200) {
-      throw new Error('Failed to fetch bored activity');
+      throw new Error('Failed to fetch bored activity')
     }
-    const activity = data.activity;
-    inputFieldEl.value = activity;
+    const activity = data.activity
+    inputFieldEl.value = activity
     
   } catch (error) {
-    console.error(error);
-    inputFieldEl.textContent = 'Failed to fetch bored activity';
+    console.error(error)
+    inputFieldEl.textContent = 'Failed to fetch bored activity'
   } finally {
     // loadingElement.style.display = 'none';
   }
-};
+}
+
 taskGeneratorBtnEl.addEventListener('click', fetchBoredActivity)
 
 function createOneConfetti(top,left){
@@ -361,7 +464,7 @@ const top = 91
 const left = Math.floor(Math.random()*50)+90
 const confet = createOneConfetti(top, left)
 document.querySelector('.bg').append(confet)
-  let degreeConf = Math.floor(Math.random()*360);
+  let degreeConf = Math.floor(Math.random()*360)
   let x = Math.floor(Math.random()*3)
   let y = Math.floor(Math.random()*3)
   let flyOfConfetti
@@ -388,7 +491,7 @@ function lunchConfetti(){
     throwConfetti()
   }
 }
-function lunchSession(){
+function lunchConfettiSession(){
   CheeringSound.play()
   let makeSession = setInterval(lunchConfetti, 2000)
   setTimeout(()=>{
